4ff1bda832da96bac13a9537e513f77d42a5eaf5
Max Sun <maxsun715@gmail.com>
2023-02-25 12:50:51 -0500

    update static files

--- api/api/settings.py
+++ api/api/settings.py
@@ -122,12 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIR = [
-    BASE_DIR / 'staticfiles'
+    BASE_DIR / 'static/'
 ]
 
 # Default primary key field type
