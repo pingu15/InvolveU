b64a5afc6ebbd04c0b1537f4aae8083ecc50eb94
Max Sun <maxsun715@gmail.com>
2023-02-25 13:23:12 -0500

    add whitenoise

--- api/api/urls.py
+++ api/api/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 from rest_framework import routers
 
