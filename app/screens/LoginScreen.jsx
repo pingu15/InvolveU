c26a25121d1052020c91ac4a4e1e94e3061a6387
Max Sun <maxsun715@gmail.com>
2023-02-26 15:25:32 -0500

    login by auth token on app

--- app/screens/LoginScreen.jsx
+++ app/screens/LoginScreen.jsx
@@ -1,25 +1,89 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import config from "../config.json";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function LoginScreen({ navigation }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginText, updateLoginText] = useState("");
+  let loggingIn = false;
 
   const handleLogin = () => {
-    // TODO: Implement login logic
-    // For example, you could use Firebase Authentication to sign in the user
-    navigation.navigate('TabNav');
+    if (loggingIn) return;
+    updateLoginText("Logging in..");
+    loggingIn = true;
+    login()
+      .then((val) => {
+        if (val == "Success!") {
+          updateLoginText(val + " Loading App...");
+          loggingIn = false;
+          updateLoginText("");
+          navigation.navigate("TabNav");
+        } else {
+          updateLoginText(val);
+          loggingIn = false;
+        }
+      })
+      .catch((err) => console.log(err));
   };
+
+  function login() {
+    return new Promise((resolve, reject) => {
+      fetch(`${config.server}/api/token/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          if (json.access && json.refresh) {
+            AsyncStorage.setItem("accesstoken", json.access)
+              .then(() => {
+                AsyncStorage.setItem("refreshtoken", json.refresh)
+                  .then(() => {
+                    resolve("Success!");
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                    resolve("Error occurred. Was storage permission denied?");
+                  });
+              })
+              .catch((err) => {
+                console.log(err);
+                resolve("Error occurred. Was storage permission denied?");
+              });
+          } else if (json.detail) {
+            resolve("Username or password incorrect");
+          } else {
+            resolve("Something went wrong. Please try again later.");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          resolve("Network error. Please try again later.");
+        });
+    });
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome!</Text>
       <TextInput
         style={styles.input}
-        placeholder="Email"
-        keyboardType="email-address"
-        value={email}
-        onChangeText={setEmail}
+        placeholder="Username"
+        onChangeText={setUsername}
       />
       <TextInput
         style={styles.input}
@@ -28,6 +92,7 @@ export default function LoginScreen({ navigation }) {
         value={password}
         onChangeText={setPassword}
       />
+      <Text style={styles.loginText}>{loginText}</Text>
       <TouchableOpacity style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
@@ -38,32 +103,35 @@ export default function LoginScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 16,
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 16,
   },
   input: {
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderRadius: 4,
     padding: 8,
     marginBottom: 16,
-    width: '100%',
+    width: "100%",
   },
   button: {
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
     padding: 8,
     borderRadius: 4,
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
   },
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
+  },
+  loginText: {
+    padding: 8,
   },
 });
