634b8f0b585b6b5d5da1b53683eb1bc39c77e51d
anieni1031 <anniewong20061031@gmail.com>
2023-02-26 21:53:24 -0500

    idk homescreen or something

--- app/screens/HomeScreen.jsx
+++ app/screens/HomeScreen.jsx
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, Image} from 'react-native';
+/* image imports */
 import Welcomeicon from '../assets/welcomeIcon.png';
+import RankIcon from '../assets/rankIcon.png';
+import UpArrow from '../assets/upArrow.png';
+
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
-SplashScreen.preventAutoHideAsync();
+//SplashScreen.preventAutoHideAsync();
 
 export default function HomeScreen() {
-<<<<<<< HEAD
-=======
-  
->>>>>>> 7631a6a1ae7ff49dadd2c01c5e660f4817bdeece
+  /*const [fontsLoaded] = useFonts({
+    'Inter': require('../assets/inter-regular-1.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }*/
+
   return (
       <View style={styles.container}>
           <Summary/>
@@ -21,46 +35,95 @@ export default function HomeScreen() {
 
 function Summary() {
   return(
-    <View style={styles.box}>
-      <Image style = {styles.icon} source={Welcomeicon}/>
-      <Text style={styles.h1}>Welcome, MaxBad!</Text>
+    <View style={[styles.box, {height: '35%'}]}>
+      <View style = {styles.row}>
+        <Image style = {styles.icon} source={Welcomeicon}/>
+        <Text style={styles.h1}>Welcome, MaxBad!</Text>
+      </View>
+      <Text style = {styles.text}>Total Points:</Text>
+      <View style = {[styles.row, {marginTop: '5%'}]}>
+        <Text style = {styles.number1}>1560</Text>
+        <Image style = {[styles.icon, {marginRight: '0%'}, {marginTop: '6%'}]} source = {UpArrow}/>
+        <Text style = {styles.percent}>25%</Text>
+      </View>
     </View>
   );
 }
 
 function Rank(){
   return(
-    <View style = {styles.box}>
-      <Text style = {styles.h1}>Rank</Text>
+    <View style = {[styles.box, {height: '40%'}, {marginTop: '0%'}]}>
+      <View style = {styles.row}>
+        <Image style = {styles.icon} source={RankIcon}/>
+        <Text style={styles.h1}>Rank</Text>
+      </View>
+      <Text style = {styles.text}>Among Grade grades:</Text>
+      <View style = {[styles.row, {marginTop: '3%'}]}>
+        <Text style = {styles.number1}>6</Text>
+        <Text style = {styles.number2}>of 247</Text>
+      </View>
+      <Text style = {styles.text}>Among Grade grades:</Text>
+      <View style = {[styles.row, {marginTop: '3%'}]}>
+        <Text style = {styles.number1}>24</Text>
+        <Text style = {styles.number2}>of 1043</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   h1: {
-    fontFamily: 'poppins',
     fontSize: 25,
     fontWeight: 'bold',
     marginBottom: 10,
     color: '#1D1E25',
     borderTopWidth: 20,
   },
+  text: {
+    fontSize: 14,
+    fontWeight: 'normal',
+    color: '#808d9e',
+    marginLeft: '5%',
+  },
+  number1: {
+    fontSize: 60,
+    fontWeight: 'bold',
+    color: '#1D1E25',
+    marginLeft: '5%'
+  },
+  number2: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#808d9e',
+    marginTop: '8%',
+    marginLeft: '5%',
+  },
+  percent: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#009c35',
+    marginTop: '7%',
+    marginLeft: '0%'
+  },
   container: {
     flexGrow: 1,
     backgroundColor: '#fafafa',
   },
   box: {
-    flexGrow: 1,
     margin: '5%',
     width: '90%',
     borderRadius: 8,
     backgroundColor: '#ffffff',
     flexDirection: 'column',
     justifyContent: 'flex-start',
-    alignItems: 'center',
+    alignItems: 'left',
+  },
+  row: {
+    flexDirection: 'row',
   },
   icon: {
     height: 36,
     width: 36,
+    margin: 15
   }
 });
