abadfe1ebef75e41d20090a416d6624ca700e45c
Max Sun <maxsun715@gmail.com>
2023-02-27 23:50:26 -0500

    sign up returns json response

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -6,17 +7,17 @@ from .models import User
 
 
 def signup(request):
-    message = {"message": "", "redirect": False}
+    message = {"message": ""}
     if request.method == "POST":
-        email = request.POST['email']
-        username = request.POST['username']
-        password = request.POST['password']
-        password2 = request.POST['password2']
-        grade = request.POST['grade']
+        data = json.loads(request.body)
+        email = data['email']
+        username = data['username']
+        password = data['password']
+        password2 = data['password2']
+        grade = data['grade']
         if password == password2:
             User.objects.create_user(email, username, password, grade)
             message['message'] = "Sign Up successful, return to app to login."
-            message['redirect'] = True
         else:
             message['message'] = "passwords do not match"
-    return render(request, "signup.html", message)
+    return JsonResponse(message)
